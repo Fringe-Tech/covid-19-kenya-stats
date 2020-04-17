51bf475d9fd791797c83f61911cc5efef4d5b94e
r3klaw <rashid.david12@gmail.com>
2020-04-17 12:35:43 +0300

    api updated v2

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, url_for
 import requests
 import json
+import sys
+import logging
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 # app._static_folder = ''
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 @app.route('/')
 def index():
-  r = requests.get('https://corona.lmao.ninja/countries/kenya')
+  r = requests.get('https://corona.lmao.ninja/v2/countries/kenya')
   return render_template('index.html', stats=json.loads(r.text))
   print("stats data: ", r.text)
 
